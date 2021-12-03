9432f466d99fc1fc9a606927486d6d4232184f9c
ViniciusTSI <carlosviniciustsi@outlook.com>
2021-12-03 18:48:08 -0300

    Entity structure

---
+++ src/main/java/one/digitalinnovation/personapi/model/Phone.java
@@ -0,0 +1,23 @@
+package one.digitalinnovation.personapi.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false, unique = true)
+    private String number;
+}
